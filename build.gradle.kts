393ffd638857ba3e157142dc664951a0a32a21b7
Pablo Sousa <ph.info.cont@rede.ulbra.br>
2025-03-17 23:18:58 -0300

    new lib added

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     testImplementation(kotlin("test"))
 }
 
